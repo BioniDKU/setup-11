0b05bad42d0588179ad784eb039756f0b588a7e2
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2023-07-09 16:30:48 +0700

    PATCHME.ps1 - Velocity IDs introduction
    Because they go with the UBRs.

--- PATCHME.ps1
+++ PATCHME.ps1
@@ -1,7 +1,8 @@
-# YuumeiDKU Windows Update mode - Update Build Revision numbers
+## YuumeiDKU - Update Build Revision numbers and ViVeTool velocity IDs listing file
 
-# The purpose of this file is so I can update the UBRs every month without having to update the whole script package.
+## The purpose of this file is so I can update the UBRs and the IDs every month without having to update the whole script package.
 
+# UBRs list
 . $workdir\modules\lib\GetEdition.ps1
 switch ($edition) {
 	
@@ -20,5 +21,18 @@ switch ($edition) {
 		<# 21H2 #>          "22000.2057"
 		<# 22H2 #>          "22621.1848"
 	)}
+	
+}
+
+# ViVeTool velocity IDs list
+switch ($build) {
+	
+	{$_ -eq 22000} {
+		$viveids = ""
+	}
+	
+	{$_ -eq 22621} {
+		$viveids = "26008830,37634385,39145991,36354489,36302090,41655236"
+	}
 	
 }
